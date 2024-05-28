ae587871a09d3382a3bea64c982936f443fa340a
dchamp16 <dchamp16@gmail.com>
2024-05-28 09:07:36 -0600

    started sturcture
    i started the UVSim but its just the structure ill continue doing the instruction.py by the way im using pycharm to run my python thats why there is a bunch of adds on

---
+++ uv_sim.py
@@ -0,0 +1,18 @@
+
+
+class UVSim:
+    def __init__(self):
+        # initialize memory, accumulator and instruction pointers
+        self.memory = [0] * 100
+        self.accumulator = 0
+        self.instruction_pointer = 0
+
+    def load_program_from_file(self, file_path):
+        # Loads a machine language program from a file into the UVSim memory
+        pass
+    def execute_program(self):
+        # Executes the machine language program loaded into the UVSim memory
+        pass
+    def handle_instruction(self, opcode, operan):
+        # handles the execution of the single instruction based on the opcode and operand
+        pass
