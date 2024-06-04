b9c07d13f9be11e1910020a5a54be1af83dd0c90
hannahansen <138264993+hannahansen@users.noreply.github.com>
2024-06-03 19:00:47 -0600

    Update utils.py
    Here are two functions that I have added. These are utility functions

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
-# if neededs
+def parse_instruction(instruction):
+    op_code = instruction // 100
+    operand = instruction % 100
+    return op_code, operand
+
+def read_program(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    program = [int(line.strip()) for line in lines]
+    return program
