f939183553fa44b823f5f66944fa0c63f7c6115c
Peter Ramos <dchamp16@gmail.com>
2024-06-05 21:11:35 -0600

    no change only whites spaces

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import utils
 def main():
     print("Enter the path to BasicML program file:")
     file_path = input()
-    
     program = utils.load_program(file_path)
     if program:
         print(f"Successfully loaded {len(program)} instructions into memory.")
     else:
         print("Failed to load program.")
         return
-    
     sim = uv_sim.UVSim()
     sim.load_program(program)
 
